9eb9d1bd6d772f0a2fbb2ada52211071e4f0d5c2
墨子熙 <zmszm666@163.com>
2022-08-19 18:02:44 +0800

    first commit

---
+++ src/JuShuiTan.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace jushuitan;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class JuShuiTan
+{
+    /**
+     * api配置
+     */
+    protected  array $config = [
+        'authUrl' => 'https://openweb.jushuitan.com/auth',
+        'baseUrl' => '',
+        'app_Key' => '',
+        'app_Secret' => '',
+        'version' => 2,
+        'charset' => 'utf-8'
+    ];
+    private $client;
+    /**
+     * 定义获取access_token接口
+     */
+    protected  string $getTokenUrl = 'https://openapi.jushuitan.com/openWeb/auth/accessToken';
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'verify' => false,
+            'headers' => [
+                'Content-Type' => 'application/x-www-form-urlencoded;charset=UTF-8'
+            ]
+        ]);
+    }
+    /**
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+
+    /**
+     * @param array $config
+     * @return JuShuiTan
+     */
+    public function setConfig(array $config): JuShuiTan
+    {
+        $this->config = $config;
+        return $this;
+    }
+
+    /**
+     * 业务授权URL拼装
+     */
+    public function createUrl(): string
+    {
+        $data = [
+            'app_key' => $this->getConfig()['app_key'],
+            'timestamp' => time(),
+            'charset' => $this->getConfig()['charset']
+        ];
+        $sign = $this->get_sign($data);
+        return $this->getConfig()['authUrl'] .
+            '?app_key=' . $data['app_key'] .
+            '&timestamp=' . $data['timestamp'] .
+            '&charset=' . $data['charset'] .
+            '&sign=' . $sign;
+    }
+
+    /**
+     * 使用code换取access_token
+     */
+    public function getAccessToken($code)
+    {
+        $data = [
+            'app_key' => $this->getConfig()['app_Key'],
+            'timestamp' => time(),
+            'grant_type' => 'authorization_code',
+            'charset' => $this->getConfig()['charset'],
+            'code' => $code,
+        ];
+        $sign = $this->get_sign($data);
+        try {
+            $data['sign'] = $sign;
+            $response =  $this->client->post($this->getTokenUrl,$data);
+            return $response->getBody();
+        } catch (GuzzleException $e) {
+            return $e;
+        }
+    }
+
+
+    /**
+     * 生成签名
+     */
+    private function get_sign($data): ?string
+    {
+        if ($data == null) {
+            return null;
+        }
+        ksort($data);
+        $result_str = "";
+        foreach ($data as $key => $val) {
+            if ($key != null && $key != "" && $key != "sign") {
+                $result_str = $result_str . $key . $val;
+            }
+        }
+        $result_str = $this->getConfig()['app_Secret']. $result_str;
+        return bin2hex(md5($result_str, true));
+    }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,125 @@
+<?php return array(
+    'root' => array(
+        'name' => 'zmoyi/jushuitan',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'guzzlehttp/guzzle' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '514f665f52a3a06775333b30b4077a647290cac4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../guzzlehttp/guzzle',
+            'aliases' => array(
+                0 => '7.4.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'guzzlehttp/promises' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a872440174bcdfd3392831de339656ac56122e26',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../guzzlehttp/promises',
+            'aliases' => array(
+                0 => '1.5.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'guzzlehttp/psr7' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '8f0a09ce19adcf28f25ff9d90e7329488501b363',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../guzzlehttp/psr7',
+            'aliases' => array(
+                0 => '2.4.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/http-client' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '22b2ef5687f43679481615605d7a15c557ce85b1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-client',
+            'aliases' => array(
+                0 => '1.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/http-client-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'psr/http-factory' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '36fa03d50ff82abcae81860bdaf4ed9a1510c7cd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-factory',
+            'aliases' => array(
+                0 => '1.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/http-factory-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'psr/http-message' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'efd67d1dc14a7ef4fc4e518e7dee91c271d524e4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/http-message',
+            'aliases' => array(
+                0 => '1.0.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'psr/http-message-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0',
+            ),
+        ),
+        'ralouphie/getallheaders' => array(
+            'pretty_version' => '3.0.3',
+            'version' => '3.0.3.0',
+            'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../ralouphie/getallheaders',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'symfony/deprecation-contracts' => array(
+            'pretty_version' => '3.0.x-dev',
+            'version' => '3.0.9999999.9999999-dev',
+            'reference' => '26954b3d62a6c5fd0ea8a2a00c0353a14978d05c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../symfony/deprecation-contracts',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'zmoyi/jushuitan' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

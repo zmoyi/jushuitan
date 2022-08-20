6bc3ab8a17fa3e73984c26118b3b10d42a99e19c
墨子熙 <zmszm666@163.com>
2022-08-20 18:26:47 +0800

    feat:新增基础api接口函数

--- src/Api/common/ServeHttp.php
+++ src/Api/common/ServeHttp.php
@@ -13,22 +13,22 @@ final class ServeHttp extends Enum
     /**
      * 店铺查询
      */
-    public const QUERY_SHOPS = 'open/shops/query';
+    private const QUERY_SHOPS = 'open/shops/query';
 
     /**
      * 物流公司查询
      */
-    public const QUERY_LOGISTICSCOMPANY = 'open/logisticscompany/query';
+    private const QUERY_LOGISTICSCOMPANY = 'open/logisticscompany/query';
 
     /**
      * 仓库查询
      */
-    public const QUERY_PARTNER_WMS = 'open/wms/partner/query';
+    private const QUERY_PARTNER_WMS = 'open/wms/partner/query';
 
     /**
      * 分销商查询列表
      */
-    public const QUERY_DISTRIBUTOR = 'open/jushuitan/distributor/query';
+    private const QUERY_DISTRIBUTOR = 'open/jushuitan/distributor/query';
 
     /**
      * 基础API路由end
@@ -39,22 +39,22 @@ final class ServeHttp extends Enum
     /**
      * 普通商品资料上传
      */
-    public const UPLOAD_ITEMSKU = '/open/jushuitan/itemsku/upload';
+    private const UPLOAD_ITEMSKU = '/open/jushuitan/itemsku/upload';
 
     /**
      * 店铺商品资料上传
      */
-    public const UPLOAD_SKUMAP = '/open/jushuitan/skumap/upload';
+    private const UPLOAD_SKUMAP = '/open/jushuitan/skumap/upload';
 
     /**
      * 组合装商品上传
      */
-    public const UPLOAD_COMBINESKU_ITEM = '/open/jushuitan/item/combinesku/upload';
+    private const UPLOAD_COMBINESKU_ITEM = '/open/jushuitan/item/combinesku/upload';
 
     /**
      * 店铺商品资料查询
      */
-    public const QUERY_SKUMAP = '/open/skumap/query';
+    private const QUERY_SKUMAP = '/open/skumap/query';
 
     /**
      * 组合装商品查询

0049fb66460090188f1a38ba3d1bd536b1213deb
墨子熙 <zmszm666@163.com>
2022-08-20 17:11:14 +0800

    feat:新增基础api接口函数

---
+++ src/Api/ApiRes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace jushuitan\Api;
+
+use jushuitan\Api\common\BaseApi;
+use jushuitan\Api\common\Client;
+use jushuitan\Api\common\ServeHttp;
+use jushuitan\Api\common\Util;
+
+class ApiRes extends BaseApi
+{
+    public function request(ServeHttp $serveHttp, $params)
+    {
+        return Client::post($serveHttp, Util::getParams($this->getConfig()['app_Secret'], $params));
+    }
+
+}
--- src/Api/common/ServeHttp.php
+++ src/Api/common/ServeHttp.php
@@ -5,6 +5,9 @@ namespace jushuitan\Api\common;
 class ServeHttp
 {
     /**
+     * 基础API路由
+     */
+    /**
      * 店铺查询
      */
     const QUERY_SHOPS = 'open/shops/query';
@@ -23,4 +26,37 @@ class ServeHttp
      * 分销商查询列表
      */
     const QUERY_DISTRIBUTOR = 'open/jushuitan/distributor/query';
+
+    /**
+     * 基础API路由end
+     * ------------------------------------------------------------------
+     * 商品API路由
+     */
+
+    /**
+     * 普通商品资料上传
+     */
+    const UPLOAD_ITEMSKU = '/open/jushuitan/itemsku/upload';
+
+    /**
+     * 店铺商品资料上传
+     */
+    const UPLOAD_SKUMAP = '/open/jushuitan/skumap/upload';
+
+    /**
+     * 组合装商品上传
+     */
+    const UPLOAD_COMBINESKU_ITEM = '/open/jushuitan/item/combinesku/upload';
+
+    /**
+     * 店铺商品资料查询
+     */
+    const QUERY_SKUMAP = '/open/skumap/query';
+
+    /**
+     * 组合装商品查询
+     */
+
+
+
 }

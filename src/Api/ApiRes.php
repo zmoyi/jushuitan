63219ecc31054f945559c42afba740209e550547
墨子熙 <zmszm666@163.com>
2022-08-22 09:54:37 +0800

    fix:调整基础api接口函数

--- src/Api/ApiRes.php
+++ src/Api/ApiRes.php
@@ -7,9 +7,9 @@ use zmoyi\JuShuiTan\Api\common\Client;
 use zmoyi\JuShuiTan\Api\common\ServeHttp;
 use zmoyi\JuShuiTan\Api\common\Util;
 
-class ApiRes extends BaseApi implements ServeHttp
+class ApiRes extends BaseApi
 {
-    public function request(ServeHttp $serveHttp, $params)
+    public function request($serveHttp, $params)
     {
         return Client::post($serveHttp, Util::getParams($this->getConfig()['app_Secret'], $params));
     }
